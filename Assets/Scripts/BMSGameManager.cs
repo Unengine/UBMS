cd314c141c3722f9211fbb828a05647f890f9cb4
unknown <unengine@gmail.com>
2019-03-28 17:12:54 +0900

    changed algo

--- Assets/Scripts/BMSGameManager.cs
+++ Assets/Scripts/BMSGameManager.cs
@@ -1,4 +1,4 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
@@ -6,7 +6,7 @@ using UnityEngine.Video;
 public class BMSGameManager : MonoBehaviour
 {
 	public bool isAuto = false;
-	public static float speed = 7f;
+	public static float speed = 3.5f;
 
 	[SerializeField]
 	private UnityEngine.UI.Text comboText;
@@ -78,18 +78,18 @@ public class BMSGameManager : MonoBehaviour
 		{
 			if (pat.Lines[i].noteList.Count > 0)
 			{
-                Note n = pat.Lines[i].noteList[pat.Lines[i].noteList.Count - 1];
-                if (judge.Judge(n, currentTime + stopTime) != JudgeType.IGNORE)
-                {
-                    if (Input.GetKeyDown(Keys[i]) && n.Extra != 1)
-                    {
-                        HandleNote(pat.Lines[i], i);
-                    }
-                    else if (Input.GetKeyUp(Keys[i]) && n.Extra == 1)
-                    {
-                        HandleNote(pat.Lines[i], i);
-                    }
-                }
+				Note n = pat.Lines[i].noteList[pat.Lines[i].noteList.Count - 1];
+				if (judge.Judge(n, currentTime) != JudgeType.IGNORE)
+				{
+					if (Input.GetKeyDown(Keys[i]) && n.Extra != 1)
+					{
+						HandleNote(pat.Lines[i], i);
+					}
+					else if (Input.GetKeyUp(Keys[i]) && n.Extra == 1)
+					{
+						HandleNote(pat.Lines[i], i);
+					}
+				}
 			}
 		}
 	}
@@ -109,7 +109,6 @@ public class BMSGameManager : MonoBehaviour
 		}
 
 		double dt = Time.fixedDeltaTime;
-		currentTime += Time.fixedDeltaTime;
 		PlayNotes();
 		if (stopTime > 0.0)
 		{
@@ -130,108 +129,108 @@ public class BMSGameManager : MonoBehaviour
 		BPM nextBPM = null;
 		bool flag = false;
 
-
-		if(pat.Stops.Count > 0)
+		if (pat.Bpms.Count > 0)
 		{
-			next = pat.Stops[pat.Stops.Count - 1];
-		}
-		if(pat.Bpms.Count > 0)
-		{
-			BPM bpm = pat.Bpms[pat.Bpms.Count - 1];
-			if(next == null) next = nextBPM = bpm;
-			else if(bpm.Beat <= next.Beat) next = nextBPM = bpm;
-
-			if(next.Timing < currentTime + dt)
+			next = nextBPM = pat.Bpms[pat.Bpms.Count - 1];
+			if (next.Timing < currentTime + dt)
 			{
 				flag = true;
 				avg = 0;
 			}
 		}
+		if (pat.Stops.Count > 0)
+		{
+			Stop stp = pat.Stops[pat.Stops.Count - 1];
+			if (next == null) next = stp;
+			else if (stp.Timing < next.Timing) next = stp;
+		}
 
 
 		double sub = 0;
 		double prevTime = currentTime;
-        while (next != null && next.Timing + stopTime < currentTime + dt)
-        {
-			if(next is BPM)
+		while (next != null && next.Timing + stopTime <= currentTime + dt)
+		{
+			if (next is BPM)
 			{
-				//Debug.Log($"change bpm to {nextBPM.Bpm} in {next.Bar}");
 				double diff = nextBPM.Timing - prevTime;
 				avg += currentBPM * diff;
 				currentBPM = nextBPM.Bpm;
 				prevTime = nextBPM.Timing;
-                pat.Bpms.RemoveAt(pat.Bpms.Count - 1);
+				pat.Bpms.RemoveAt(pat.Bpms.Count - 1);
 			}
-			if(next is Stop)
+			if (next is Stop)
 			{
-				//Debug.Log($"stop in {next.Bar}");
 				double duration = pat.StopDurations[(next as Stop).Key] / currentBPM * 240;
 				stopTime += duration;
 				pat.Stops.RemoveAt(pat.Stops.Count - 1);
-				if(next.Timing + stopTime >= currentTime + dt)
+
+				if (next.Timing + stopTime >= currentTime + dt)
 				{
 					double diff = currentTime + dt - next.Timing;
 					sub += diff;
 					stopTime -= diff;
 					break;
 				}
+				else
+				{
+					sub += stopTime;
+					stopTime = 0;
+				}
 			}
 
 			next = null;
-
-            if (pat.Stops.Count > 0)
-            {
-                next = pat.Stops[pat.Stops.Count - 1];
-            }
-            if (pat.Bpms.Count > 0)
-            {
-                BPM bpm = pat.Bpms[pat.Bpms.Count - 1];
-                if (next == null) next = nextBPM = bpm;
-                else if (bpm.Beat <= next.Beat) next = nextBPM = bpm;
-            }
-        }
+			if (pat.Bpms.Count > 0)
+			{
+				next = nextBPM = pat.Bpms[pat.Bpms.Count - 1];
+			}
+			if (pat.Stops.Count > 0)
+			{
+				Stop stp = pat.Stops[pat.Stops.Count - 1];
+				if (next == null) next = stp;
+				else if (stp.Timing < next.Timing) next = stp;
+			}
+		}
 
 		dt -= sub;
-		if(dt < 0) Debug.Log($"fuck, {dt}");
-        if (flag && prevTime <= currentTime + dt)
-        {
-            avg += currentBPM * (currentTime + dt - prevTime);
-        }
+		if (dt < 0) Debug.Log($"dt is negative!, {dt}");
+		if (flag && prevTime <= currentTime + dt)
+		{
+			avg += currentBPM * (currentTime + dt - prevTime);
+		}
 
 		avg /= 60;
 		currentBeat += avg;
-		//currentTime += dt;
+		currentTime += dt;
 		scroll += avg * speed;
 		noteParent.transform.position = new Vector3(0.0f, (float)-scroll, 0.0f);
 		//손실을 적게 일어나게 하기 위해 누적된 double을 float로 변환.
 	}
-
 	private void Damage(Line l)
 	{
 		List<Note> list = l.landMineList;
 		int idx = list.Count - 1;
 
-        Note n = list[idx];
-        if (n.Extra == -1 && judge.Judge(n, currentTime + stopTime) == JudgeType.PGREAT)
-        {
-            Debug.Log("bomb");
-            comboText.text = "LANDMINE!";
-            comboAnim.Rebind();
-            comboAnim.Play("ComboUp");
-            n.Extra = -2;
-        }
-		
+		Note n = list[idx];
+		if (n.Extra == -1 && judge.Judge(n, currentTime) == JudgeType.PGREAT)
+		{
+			Debug.Log("bomb");
+			comboText.text = "LANDMINE!";
+			comboAnim.Rebind();
+			comboAnim.Play("ComboUp");
+			n.Extra = -2;
+		}
+
 	}
 
 	private void HandleNote(Line l, int idx, float volume = 1.0f)
 	{
 		if (l.noteList.Count <= 0) return;
-        Note n = l.noteList[l.noteList.Count - 1];
-        n.Model.SetActive(false);
-        l.noteList.RemoveAt(l.noteList.Count - 1);
+		Note n = l.noteList[l.noteList.Count - 1];
+		n.Model.SetActive(false);
+		l.noteList.RemoveAt(l.noteList.Count - 1);
 
 		sm.PlayKeySound(n.KeySound, volume);
-		JudgeType result = judge.Judge(n, currentTime + stopTime);
+		JudgeType result = judge.Judge(n, currentTime);
 		if (result > JudgeType.BAD)
 		{
 			comboText.text = result + " " + ++combo;
@@ -254,7 +253,7 @@ public class BMSGameManager : MonoBehaviour
 	private void PlayNotes()
 	{
 
-        while (pat.BGSounds.Count > 0 && pat.BGSounds[pat.BGSounds.Count - 1].Timing <= currentTime)
+		while (pat.BGSounds.Count > 0 && pat.BGSounds[pat.BGSounds.Count - 1].Timing <= currentTime)
 		{
 
 			int keySound = pat.BGSounds[pat.BGSounds.Count - 1].KeySound;
@@ -271,13 +270,13 @@ public class BMSGameManager : MonoBehaviour
 				{
 					HandleNote(l, i);
 				}
-				
+
 				while (l.landMineList.Count > 0 &&
 				judge.Judge(l.landMineList[l.landMineList.Count - 1], currentTime) == JudgeType.POOR)
 				{
-                    Note n = l.landMineList[l.landMineList.Count - 1];
-                    n.Model.SetActive(false);
-                    l.landMineList.RemoveAt(l.landMineList.Count - 1);
+					Note n = l.landMineList[l.landMineList.Count - 1];
+					n.Model.SetActive(false);
+					l.landMineList.RemoveAt(l.landMineList.Count - 1);
 				}
 			}
 		}
@@ -286,21 +285,21 @@ public class BMSGameManager : MonoBehaviour
 			for (int i = 0; i < pat.Lines.Length; ++i)
 			{
 				Line l = pat.Lines[i];
-            	if (Input.GetKey(Keys[i]) && l.landMineList.Count > 0)
-                {
-                    Damage(l);
-                }
+				if (Input.GetKey(Keys[i]) && l.landMineList.Count > 0)
+				{
+					Damage(l);
+				}
 				while (l.noteList.Count > 0 && judge.Judge(l.noteList[l.noteList.Count - 1], currentTime) == JudgeType.POOR)
 				{
 					HandleNote(l, i, 0.3f);
 				}
-				
-                while (l.landMineList.Count > 0 &&
+
+				while (l.landMineList.Count > 0 &&
 				judge.Judge(l.landMineList[l.landMineList.Count - 1], currentTime) == JudgeType.POOR)
 				{
-                    Note n = l.landMineList[l.landMineList.Count - 1];
-                    n.Model.SetActive(false);
-                    l.landMineList.RemoveAt(l.landMineList.Count - 1);
+					Note n = l.landMineList[l.landMineList.Count - 1];
+					n.Model.SetActive(false);
+					l.landMineList.RemoveAt(l.landMineList.Count - 1);
 				}
 			}
 		}
--- Assets/Scripts/BMSPattern.cs
+++ Assets/Scripts/BMSPattern.cs
@@ -98,215 +98,225 @@ public class Stop : BMSObject
     }
 }
 
-public class BMSPattern {
+public class BMSPattern
+{
 
-    public int BarCount { get; set; } = 0;
-    public List<BGAChange> BGAChanges { get; set; }
-    public List<Note> BGSounds { get; set; }
-    public List<string> KeySounds { get; set; }
-    public List<BPM> Bpms { get; set; }
-    public List<Stop> Stops { set; get; }
-    public List<double> LegacyStopDurations { get; set; }
+	public int BarCount { get; set; } = 0;
+	public List<BGAChange> BGAChanges { get; set; }
+	public List<Note> BGSounds { get; set; }
+	public List<string> KeySounds { get; set; }
+	public List<BPM> Bpms { get; set; }
+	public List<Stop> Stops { set; get; }
+	public List<double> LegacyStopDurations { get; set; }
 	public Dictionary<string, double> StopDurations { get; set; }
-    public Dictionary<int, double> BeatCTable { get; set; }
-    public Line[] Lines { get; set; }
+	public Dictionary<int, double> BeatCTable { get; set; }
+	public Line[] Lines { get; set; }
 
-    public BMSPattern()
-    {
-        BeatCTable = new Dictionary<int, double>();
+	public BMSPattern()
+	{
+		BeatCTable = new Dictionary<int, double>();
 		StopDurations = new Dictionary<string, double>();
-        LegacyStopDurations = new List<double>()
-        {
-            Capacity = 5
-        };
-        Stops = new List<Stop>()
-        {
-            Capacity = 5
-        };
-        Bpms = new List<BPM>()
-        {
-            Capacity = 5
-        };
-        KeySounds = new List<string>()
-        {
-            Capacity = 300
-        };
-        BGAChanges = new List<BGAChange>()
-        {
-            Capacity = 10
-        };
-        BGSounds = new List<Note>();
-        Lines = new Line[9];
-        for (int i = 0; i < 9; ++i) Lines[i] = new Line();
-    }
+		LegacyStopDurations = new List<double>()
+		{
+			Capacity = 5
+		};
+		Stops = new List<Stop>()
+		{
+			Capacity = 5
+		};
+		Bpms = new List<BPM>()
+		{
+			Capacity = 5
+		};
+		KeySounds = new List<string>()
+		{
+			Capacity = 300
+		};
+		BGAChanges = new List<BGAChange>()
+		{
+			Capacity = 10
+		};
+		BGSounds = new List<Note>();
+		Lines = new Line[9];
+		for (int i = 0; i < 9; ++i) Lines[i] = new Line();
+	}
 
-    public void AddBGAChange(int bar, int beat, int beatLength, int idx, bool isPic = false)
-    {
-        BGAChanges.Add(new BGAChange(bar, idx, beat, beatLength, isPic));
-    }
+	public void AddBGAChange(int bar, int beat, int beatLength, int idx, bool isPic = false)
+	{
+		BGAChanges.Add(new BGAChange(bar, idx, beat, beatLength, isPic));
+	}
 
-    public void AddNote(int line, int bar, double beat, double beatLength, int keySound, int extra)
-    {
-        if(extra == -1) Lines[line].landMineList.Add(new Note(bar, keySound, beat, beatLength, extra));
-        else Lines[line].noteList.Add(new Note(bar, keySound, beat, beatLength, extra));
-    }
+	public void AddNote(int line, int bar, double beat, double beatLength, int keySound, int extra)
+	{
+		if (extra == -1) Lines[line].landMineList.Add(new Note(bar, keySound, beat, beatLength, extra));
+		else Lines[line].noteList.Add(new Note(bar, keySound, beat, beatLength, extra));
+	}
 
-    public void AddBGSound(int bar, double beat, double beatLength, int keySound)
-    {
-        BGSounds.Add(new Note(bar, keySound, beat, beatLength, 0));
-    }
+	public void AddBGSound(int bar, double beat, double beatLength, int keySound)
+	{
+		BGSounds.Add(new Note(bar, keySound, beat, beatLength, 0));
+	}
 
-    public void AddNewBeatC(int bar, double beatC)
-    {
-        BeatCTable.Add(bar, beatC);
-    }
+	public void AddNewBeatC(int bar, double beatC)
+	{
+		BeatCTable.Add(bar, beatC);
+	}
 
-    public void AddBPM(int bar, double beat, double beatLength, double bpm)
-    {
-        Bpms.Add(new BPM(bar, bpm, beat, beatLength));
-    }
+	public void AddBPM(int bar, double beat, double beatLength, double bpm)
+	{
+		Bpms.Add(new BPM(bar, bpm, beat, beatLength));
+	}
 
 	public void AddBPM(BPM bpm)
 	{
 		Bpms.Add(bpm);
 	}
 
-    public void AddStop(int bar, double beat, double beatLength, string key)
-    {
-        Stops.Add(new Stop(bar, key, beat, beatLength));
-    }
+	public void AddStop(int bar, double beat, double beatLength, string key)
+	{
+		Stops.Add(new Stop(bar, key, beat, beatLength));
+	}
 
-    public double GetPreviousBarBeatSum(int bar)
-    {
+	public double GetPreviousBarBeatSum(int bar)
+	{
 		double sum = 0;
-        for (int i = 0; i < bar; ++i)
-        {
-            sum += 4.0 * GetBeatC(i);
-        }
-        return sum;
-    }
+		for (int i = 0; i < bar; ++i)
+		{
+			sum += 4.0 * GetBeatC(i);
+		}
+		return sum;
+	}
 
-    private double GetTiming(Note note)
-    {
-        BPM prev = Bpms[Bpms.Count - 1];
+	private double GetTiming(Note note)
+	{
+		BPM prev = Bpms[Bpms.Count - 1];
 		double sum = 0;
-        for (int i = Bpms.Count - 1; i >= 0; --i)
-        {
-            double nextBeat = Bpms[i - 1].Beat;
+		for (int i = Bpms.Count - 1; i >= 0; --i)
+		{
+			double nextBeat = Bpms[i - 1].Beat;
 
-            prev = Bpms[i - 1];
-        }
-        return sum;
-    }
+			prev = Bpms[i - 1];
+		}
+		return sum;
+	}
 
 	public double GetBeatC(int bar) => BeatCTable.ContainsKey(bar) ? BeatCTable[bar] : 1.0;
 
-    public void CalCulateBeats()
-    {
-        Debug.Log("Calc");
+	public void CalCulateBeats()
+	{
+		Debug.Log("Calc");
 
-        foreach (BPM b in Bpms)
-        {
-            b.CalculateBeat(GetPreviousBarBeatSum(b.Bar), GetBeatC(b.Bar));
-        }
-        Bpms.Sort();
-        if (Bpms.Count == 0 || (Bpms.Count > 0 && Bpms[Bpms.Count - 1].Beat != 0))
-            AddBPM(0, 0, 1, BMSParser.instance.header.Bpm);
+		foreach (BPM b in Bpms)
+		{
+			b.CalculateBeat(GetPreviousBarBeatSum(b.Bar), GetBeatC(b.Bar));
+		}
+		Bpms.Sort();
+		if (Bpms.Count == 0 || (Bpms.Count > 0 && Bpms[Bpms.Count - 1].Beat != 0))
+			AddBPM(0, 0, 1, BMSParser.instance.header.Bpm);
 
-        Bpms[Bpms.Count - 1].Timing = 0;
-        for (int i = Bpms.Count - 2; i > -1; --i)
-        {
-            Bpms[i].Timing = Bpms[i + 1].Timing + (Bpms[i].Beat - Bpms[i + 1].Beat) / (Bpms[i + 1].Bpm / 60);
-        }
+		Bpms[Bpms.Count - 1].Timing = 0;
+		for (int i = Bpms.Count - 2; i > -1; --i)
+		{
+			Bpms[i].Timing = Bpms[i + 1].Timing + (Bpms[i].Beat - Bpms[i + 1].Beat) / (Bpms[i + 1].Bpm / 60);
+		}
 
-        foreach (Stop s in Stops)
-        {
-            s.CalculateBeat(GetPreviousBarBeatSum(s.Bar), GetBeatC(s.Bar));
-            s.Timing = GetTimingInSecond(s);
-        }
-        Stops.Sort();
+		foreach (Stop s in Stops)
+		{
+			s.CalculateBeat(GetPreviousBarBeatSum(s.Bar), GetBeatC(s.Bar));
+			s.Timing = GetTimingInSecond(s);
+		}
+		Stops.Sort();
 
-        foreach(BGAChange c in BGAChanges)
-        {
-            c.CalculateBeat(GetPreviousBarBeatSum(c.Bar), GetBeatC(c.Bar));
-        }
-        BGAChanges.Sort();
+		foreach (BGAChange c in BGAChanges)
+		{
+			c.CalculateBeat(GetPreviousBarBeatSum(c.Bar), GetBeatC(c.Bar));
+		}
+		BGAChanges.Sort();
 
-        foreach (Note n in BGSounds)
-        {
-            n.CalculateBeat(GetPreviousBarBeatSum(n.Bar), GetBeatC(n.Bar));
-            n.Timing = GetTimingInSecond(n);
-        }
-        BGSounds.Sort();
+		foreach (Note n in BGSounds)
+		{
+			n.CalculateBeat(GetPreviousBarBeatSum(n.Bar), GetBeatC(n.Bar));
+			n.Timing = GetTimingInSecond(n);
+		}
+		BGSounds.Sort();
 
-        foreach (Line l in Lines)
-        {
+		foreach (Line l in Lines)
+		{
 			int idx = Stops.Count - 1;
 
-            foreach (Note n in l.landMineList)
-            {
-                n.CalculateBeat(GetPreviousBarBeatSum(n.Bar), GetBeatC(n.Bar));
+			foreach (Note n in l.landMineList)
+			{
+				n.CalculateBeat(GetPreviousBarBeatSum(n.Bar), GetBeatC(n.Bar));
 				n.Timing = GetTimingInSecond(n);
-            }
-            foreach (Note n in l.noteList)
-            {
-                n.CalculateBeat(GetPreviousBarBeatSum(n.Bar), GetBeatC(n.Bar));
+			}
+			foreach (Note n in l.noteList)
+			{
+				n.CalculateBeat(GetPreviousBarBeatSum(n.Bar), GetBeatC(n.Bar));
 				n.Timing = GetTimingInSecond(n);
-            }
-            l.noteList.Sort();
-            l.landMineList.Sort();
-        }
+			}
+			l.noteList.Sort();
+			l.landMineList.Sort();
+		}
 
-        foreach (Line l in Lines)
-        {
-            for (int i = l.noteList.Count - 1; i > -1; --i)
-            {
-                Note n = l.noteList[i];
-                int idx = Stops.Count;
-                double sum = 0;
-                while(idx > 0 && n.Beat > Stops[--idx].Beat) sum += StopDurations[Stops[idx].Key] / GetBPM(Stops[idx].Beat) * 240;
-                n.Timing += sum;
-            }
+		//foreach (Line l in Lines)
+		//{
+		//	for (int i = l.noteList.Count - 1; i > -1; --i)
+		//	{
+		//		Note n = l.noteList[i];
+		//		int idx = Stops.Count;
+		//		double sum = 0;
+		//		while (idx > 0 && n.Beat > Stops[--idx].Beat) sum += StopDurations[Stops[idx].Key] / GetBPM(Stops[idx].Beat) * 240;
+		//		n.Timing += sum;
+		//	}
 
-            for (int i = l.landMineList.Count - 1; i > -1; --i)
-            {
-                Note n = l.landMineList[i];
-                int idx = Stops.Count;
-                double sum = 0;
-                while(idx > 0 && n.Beat > Stops[--idx].Beat) sum += StopDurations[Stops[idx].Key] / GetBPM(Stops[idx].Beat) * 240;
-                n.Timing += sum;
-            }
-        }
+		//	for (int i = l.landMineList.Count - 1; i > -1; --i)
+		//	{
+		//		Note n = l.landMineList[i];
+		//		int idx = Stops.Count;
+		//		double sum = 0;
+		//		while (idx > 0 && n.Beat > Stops[--idx].Beat) sum += StopDurations[Stops[idx].Key] / GetBPM(Stops[idx].Beat) * 240;
+		//		n.Timing += sum;
+		//	}
+		//}
 
-        for (int i = BGSounds.Count - 1; i > -1; --i)
-        {
-            Note n = BGSounds[i];
-            int idx = Stops.Count;
-            double sum = 0;
-            while (idx > 0 && n.Beat > Stops[--idx].Beat) sum += StopDurations[Stops[idx].Key] / GetBPM(Stops[idx].Beat) * 240;
-            n.Timing += sum;
-        }
-    }
+		//for (int i = BGSounds.Count - 1; i > -1; --i)
+		//{
+		//	Note n = BGSounds[i];
+		//	int idx = Stops.Count;
+		//	double sum = 0;
+		//	while (idx > 0 && n.Beat > Stops[--idx].Beat) sum += StopDurations[Stops[idx].Key] / GetBPM(Stops[idx].Beat) * 240;
+		//	n.Timing += sum;
+		//}
 
-    private double GetBPM(double beat)
-    {
-        if (Bpms.Count == 1) return Bpms[0].Bpm;
-        int idx = Bpms.Count - 1;
-        while (idx > 0 && beat >= Bpms[--idx].Beat) ;
-        return Bpms[idx + 1].Bpm;
-    }
+		//for (int i = Bpms.Count - 1; i > -1; --i)
+		//{
+		//	BPM b = Bpms[i];
+		//	int idx = Stops.Count;
+		//	double sum = 0;
+		//	while (idx > 0 && b.Beat > Stops[--idx].Beat) sum += StopDurations[Stops[idx].Key] / GetBPM(Stops[idx].Beat) * 240;
+		//	b.Timing += sum;
+		//}
+	}
 
-    private double GetTimingInSecond(BMSObject obj)
-    {
-        double timing = 0;
-        int i;
-        for (i = Bpms.Count - 1; i > 0 && obj.Beat > Bpms[i - 1].Beat; --i)
-        {
-            timing += (Bpms[i - 1].Beat - Bpms[i].Beat) / Bpms[i].Bpm * 60;
-        }
-        timing += (obj.Beat - Bpms[i].Beat) / Bpms[i].Bpm * 60;
-        return timing;
-    }
+	private double GetBPM(double beat)
+	{
+		if (Bpms.Count == 1) return Bpms[0].Bpm;
+		int idx = Bpms.Count - 1;
+		while (idx > 0 && beat >= Bpms[--idx].Beat) ;
+		return Bpms[idx + 1].Bpm;
+	}
+
+	private double GetTimingInSecond(BMSObject obj)
+	{
+		double timing = 0;
+		int i;
+		for (i = Bpms.Count - 1; i > 0 && obj.Beat > Bpms[i - 1].Beat; --i)
+		{
+			timing += (Bpms[i - 1].Beat - Bpms[i].Beat) / Bpms[i].Bpm * 60;
+		}
+		timing += (obj.Beat - Bpms[i].Beat) / Bpms[i].Bpm * 60;
+		return timing;
+	}
 
 	public BMSObject Peek(List<BMSObject> list) => list[list.Count - 1];
 }
